77554189ca52ccd99131286c2d38637433a7d288
Yann Claudon <yann.claudon@gmail.com>
2022-02-28 02:17:00 +0100

    Géolocalisation de l'utilisateur

--- Application/src/App.js
+++ Application/src/App.js
@@ -4,13 +4,10 @@ import MapboxGL from "@react-native-mapbox-gl/maps";
 
 MapboxGL.setAccessToken('pk.eyJ1IjoicHJvamV0dmlsbGUiLCJhIjoiY2t3YXAycTVqM2Q1aDJ1cm9qcnZyeWVyZyJ9.4nROH940huv29v7CqnFPyw');
 
+const IS_ANDROID = Platform.OS === 'android';
+
 const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
+  
   container: {
     height: "100%",
     width: "100%",
@@ -21,17 +18,31 @@ const styles = StyleSheet.create({
 });
 
 export default class App extends Component {
+
+  async UNSAFE_componentWillMount() {
+
+    if (IS_ANDROID) {
+      const isGranted = await MapboxGL.requestAndroidLocationPermissions();
+      this.setState({
+        isAndroidPermissionGranted: isGranted,
+        isFetchingAndroidPermission: false,
+      });
+    }
+  }
+
   componentDidMount() {
     MapboxGL.setTelemetryEnabled(true);
   }
 
   render() {
     return (
-      <View style={styles.page}>
+      
         <View style={styles.container}>
-          <MapboxGL.MapView style={styles.map} styleURL="mapbox://styles/projetville/ckysfg35o0wyx14mikay588x3" /*centerCoordinate={this.state.coordinates[0]} showUserLocation={true} userTrackingMode={this.state.userSelectedUserTrackingMode}*//>
+          <MapboxGL.MapView style={styles.map} styleURL="mapbox://styles/projetville/ckysfg35o0wyx14mikay588x3">
+            <MapboxGL.UserLocation visible={true} showsUserHeadingIndicator={true}/>
+          </MapboxGL.MapView>
         </View>
-      </View>
+
     );
   }
 }
