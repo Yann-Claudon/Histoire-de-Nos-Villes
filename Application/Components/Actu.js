9cd3654186482c86c33ee36fa3e55d2d654c3aea
Mathias Morel <m.morel@cs2i-bourgogne.com>
2022-03-25 19:24:20 +0100

    Fontion Display Notif mais en sdk 31 et PAS 30 !!!

--- Application/Components/Actu.js
+++ Application/Components/Actu.js
@@ -1,13 +1,34 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text,Button } from "react-native";
 import Style from "../Style";
 import Haut from "./Haut";
+import notifee from '@notifee/react-native';
+
 export default class Actu extends Component {
+
+    async onDisplayNotification() {
+        // Create a channel
+        const channelId = await notifee.createChannel({
+          id: 'default',
+          name: 'Default Channel',
+        });
+    
+        // Display a notification
+        await notifee.displayNotification({
+          title: 'MANGE TES MORTS',
+          body: 'COUCOU LES MUSLIMS',
+          android: {
+            channelId,
+            smallIcon: 'ic_launcher', // optional, defaults to 'ic_launcher'.
+          },
+        });
+    }
     render(){
         return (
             <View>
                 <Haut></Haut>
                 <Text style={Style.typoTitre}>Actualités</Text>
+                <Button title="CLIQUE MOI !" onPress={this.onDisplayNotification}></Button>
             </View>
         );
     }
